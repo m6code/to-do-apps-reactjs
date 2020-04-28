5883954fe5f9117915ac8cbba59b378668b025c7
M6code <benjamynfrank101@gmail.com>
2020-04-28 12:41:38 +0100

    Add ability to create new items in the todo list

--- to-do-digitalocean/src/App.js
+++ to-do-digitalocean/src/App.js
@@ -1,16 +1,22 @@
 import React, { useState } from 'react';
 import './App.css';
 import { Todo } from './components/Todo';
+import TodoForm from './components/TodoForm';
 
 function App() {
   // The first parameter, todos, is what we are going to name our state.
   // The second parameter, setTodos, is what we are going to use to set the state.
-  // eslint-disable-next-line
   const [todos, setTodos] = useState([
     { text: "Learn about React" },
     { text: "Meet friend for Lunch" },
     { text: "Build a cool todo app" }
   ]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, {text}];
+    setTodos(newTodos);
+  }
+
   //TODO: Render Todos here
   return (
     <div className="app">
@@ -22,6 +28,7 @@ function App() {
             todo={todo}
           />
         ))}
+        <TodoForm addTodo={addTodo} />
       </div>
     </div>
   );
