2fa4f4e516c4193f40bb353387ec5d83e37520fd
M6code <benjamynfrank101@gmail.com>
2020-04-28 11:45:51 +0100

    Reading a list of items

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 A Repository Containing some TodoApps built with Reactjs for learning purpose.
+
+## 1. [TODO App DigitalOcean Tutorial](https://www.digitalocean.com/community/tutorials/how-to-build-a-react-to-do-app-with-react-hooks)
--- to-do-digitalocean/src/App.js
+++ to-do-digitalocean/src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { Todo } from './components/Todo';
 
 function App() {
+  // The first parameter, todos, is what we are going to name our state.
+  // The second parameter, setTodos, is what we are going to use to set the state.
+  // eslint-disable-next-line
+  const [todos, setTodos] = useState([
+    { text: "Learn about React" },
+    { text: "Meet friend for Lunch" },
+    { text: "Build a cool todo app" }
+  ]);
+  //TODO: Render Todos here
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+          />
+        ))}
+      </div>
     </div>
   );
 }
+
 
 export default App;
