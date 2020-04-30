f532dcf14595db28d57086cfaaa572d6c10e23d1
M6code <benjamynfrank101@gmail.com>
2020-04-30 15:27:39 +0100

    Add materialize css

--- to-do-digitalocean/src/App.js
+++ to-do-digitalocean/src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './assets/css/materialize.min.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col s12">
+          <div className="card horizontal center">
+            <div className="card-stacked">
+              <div className="card-content">
+                  <h3>ToDo App</h3>
+                  <p className="flow-text">A Simple Todo App Using React js from DigitalOcean tutorial</p>
+
+              </div>
+              <div className="card-action"><p  className="">Our Todo List should be rendered here</p> </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
