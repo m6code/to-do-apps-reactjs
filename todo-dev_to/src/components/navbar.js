f003cb30b23a5ef96e7c12f95ae4f5571c28a88b
M6code <benjamynfrank101@gmail.com>
2020-05-06 02:19:29 +0100

    Add ability to show todo form in smaller screen

--- todo-dev_to/src/components/navbar.js
+++ todo-dev_to/src/components/navbar.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Navbar() {
+    const [menu, setMenu] = useState(false);
+
     return (
         <div className="navbar has-shadow">
             <div className="navbar-brand">
                 <div className="navbar-item">
                     <p className="title">Todo App with Dev.to</p>
                 </div>
-                <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                <a role="button" onClick={() => setMenu(!menu)} className={`navbar-burger burger ${menu ? "is-active" : ""}`} aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
                     <span aria-hidden="true"></span>
                     <span aria-hidden="true"></span>
                     <span aria-hidden="true"></span>
@@ -15,7 +17,7 @@ export default function Navbar() {
             </div>
 
 
-            <div className="navbar-menu">
+            <div className={`navbar-menu ${menu ? "is-active" : ""}`}>
                 <div className="navbar-end">
                     <div className="navbar-item">
                         <div className="field has-addons">
