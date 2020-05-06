bd183100c70c368fd318756ce6b606338620ec0d
M6code <benjamynfrank101@gmail.com>
2020-05-06 09:18:15 +0100

    Add and delete todo | display added todo on todolist component

--- todo-dev_to/src/components/todo.js
+++ todo-dev_to/src/components/todo.js
@@ -12,8 +12,8 @@ function Todo(props) {
                     </div>
                     <div className="level-right">
                         <div className="level-item buttons">
-                            <button className={`button has-text-weight-bold ${props.todo.isDone ? "is-warning" : "is-primary"}`}>{props.todo.isDone ? "Undo" : "Done"}</button>
-                            <button className="button is-danger has-text-weight-bold">Delete</button>
+                            <button onClick={props.toggleDone.bind(this, props.todo)} className={`button has-text-weight-bold ${props.todo.isDone ? "is-warning" : "is-primary"}`}>{props.todo.isDone ? "Undo" : "Done"}</button>
+                            <button onClick={props.deleteTodo.bind(this, props.todo)} className="button is-danger has-text-weight-bold">Delete</button>
                         </div>
                     </div>
                 </div>
