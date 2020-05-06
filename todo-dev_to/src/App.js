d23f067a5b01bf3eaec0ef8ee7724a38d2809537
M6code <benjamynfrank101@gmail.com>
2020-05-06 02:43:40 +0100

    Add a todolist component

--- todo-dev_to/src/App.js
+++ todo-dev_to/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './components/navbar';
+import TodoList from './components/todolist';
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <TodoList/>
     </div>
   );
 }
---
+++ todo-dev_to/src/components/todolist.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function TodoList() {
+    return (
+        <div>
+            <div className="hero is-info">
+                <div className="hero-body has-text-centered">
+                    <p className="title is-1">Todos</p>
+                </div>
+            </div>
+
+            <section className="section">
+                <div className="container">
+                    Todos will show up here later
+                </div>
+            </section>
+        </div>
+    )
+}
