b55648425dc192b6f52bf4c8909a365ad1680889
M6code <benjamynfrank101@gmail.com>
2020-05-07 13:08:04 +0100

    Add and display todo items

--- todoapp_3_yt/src/App.js
+++ todoapp_3_yt/src/App.js
@@ -1,16 +1,67 @@
 import React from 'react';
 import './App.css';
+import ListItems from './components/ListItems';
+
+
+//library.add(faTrash);
 
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: [],
+      currentItem: {
+        text: "",
+        key: ""
+      }
+    }
+    this.handleInput = this.handleInput.bind(this);
+    this.addItem = this.addItem.bind(this);
+
+  }
+
+  handleInput(e) {
+    this.setState({
+      currentItem: {
+        text: e.target.value,
+        key: Date.now()
+      }
+    })
+  }
+
+  addItem(e) {
+    e.preventDefault();
+    const newItem = this.state.currentItem;
+    console.log(newItem);
+    if (newItem.text !== "") {
+      const nuItems = [...this.state.items, newItem];
+      this.setState({
+        items: nuItems,
+        currentItem: {
+          text: "",
+          key: ""
+        }
+      })
+    }
+  }
+
+
   render() {
     return (
       <div className="App">
-      <header>
-        <form id='to-do-form'>
-          <input type='text' placeholder='Enter Text'/>
-          <button type='submit'>Add Todo</button>
-        </form>
-      </header>
+        <header>
+          <form id='to-do-form' onSubmit={this.addItem}>
+            <input
+              type='text'
+              placeholder='Enter Text'
+              value={this.state.currentItem.text}
+              onChange={this.handleInput} />
+            <button type='submit'>Add Todo</button>
+          </form>
+        </header>
+        <ListItems items={this.state.items} />
       </div>
     )
   }
