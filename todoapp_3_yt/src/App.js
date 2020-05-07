977bcf00c4fc71a9bc5999071ef175a7d1967ee4
M6code <benjamynfrank101@gmail.com>
2020-05-07 12:11:21 +0100

    Create and style form to add todo items

--- todoapp_3_yt/src/App.js
+++ todoapp_3_yt/src/App.js
@@ -4,8 +4,13 @@ import './App.css';
 export default class App extends React.Component {
   render() {
     return (
-      <div>
-        Hello World
+      <div className="App">
+      <header>
+        <form id='to-do-form'>
+          <input type='text' placeholder='Enter Text'/>
+          <button type='submit'>Add Todo</button>
+        </form>
+      </header>
       </div>
     )
   }
