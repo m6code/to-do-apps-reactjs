2d8b94b9592cbf8b6f8999c90c868d3a52dc9a93
M6code <benjamynfrank101@gmail.com>
2020-05-07 13:58:40 +0100

    Add animation of listItems with react-flip-move

--- todoapp_3_yt/src/components/ListItems.js
+++ todoapp_3_yt/src/components/ListItems.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import '../App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import FlipMove from 'react-flip-move';
 
 export default function ListItems(props) {
     const items = props.items;
     const listItems = items.map(item => {
         return <div className="list" key={item.key}>
             <p>
-            <input
-                type="text"
-                id={item.key}
-                value={item.text}    
-                onChange={(e) =>{
-                    props.update(e.target.value, item.key)
-                }}        
-            />
+                <input
+                    type="text"
+                    id={item.key}
+                    value={item.text}
+                    onChange={(e) => {
+                        props.update(e.target.value, item.key)
+                    }}
+                />
                 <span>
                     <FontAwesomeIcon
                         className="faicons"
@@ -26,5 +27,12 @@ export default function ListItems(props) {
         </div>
     })
 
-    return <div>{listItems}</div>
+    return (
+        <div>
+            <FlipMove duration={400} easing="ease-in-out">
+                {listItems}
+            </FlipMove>
+
+        </div>
+    )
 }
